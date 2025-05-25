61c936407f9c75a51b3a9f0984e330bad12b6c29
Wassim Saioudi <92161736+2va2s@users.noreply.github.com>
2025-05-25 20:47:20 +0200

    Update chroma_client.py

--- src/chroma_client.py
+++ src/chroma_client.py
@@ -4,7 +4,7 @@ import sys
 import pysqlite3
 sys.modules["sqlite3"] = sys.modules.pop("pysqlite3")
 
-os.environ["CHROMA_DB_IMPL"] = "duckdb+parquet"
+# os.environ["CHROMA_DB_IMPL"] = "duckdb+parquet"
 
 from chromadb import Client
 from chromadb.config import Settings
